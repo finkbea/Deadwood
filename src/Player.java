7d70282c2ec5106edb595307e96b348f2b517185
jonesmt <jonesmt@wwu.edu>
2019-02-12 12:44:31 -0800

    added initial rank, credits, dollars

--- src/Player.java
+++ src/Player.java
@@ -2,9 +2,15 @@
 public class Player{
 
     private int pid;
+    private int dollars;
+    private int credits;
+    private int rank;
     
     Player(int p_id){
 	pid = p_id;
+	dollars = 0;
+	credits = 0;
+	rank = 1;
 	System.out.println("Created player with pid of "+p_id);
     }
 
