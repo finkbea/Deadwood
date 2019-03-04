d97d72465fb1a8c29d5cf557bae977ebbc65dae2
jonesmt <jonesmt@wwu.edu>
2019-03-03 17:52:31 -0800

    moved shotCounterPanel creation into new file

---
+++ src/CreateShotCountersPanels.java
@@ -0,0 +1,103 @@
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.util.*;
+import java.awt.BorderLayout;
+
+public class CreateShotCountersPanels{
+
+    public static void main(JFrame mainFrame){
+	JPanel shotCounter1 = new JPanel();
+	shotCounter1.setBounds(236, 11, 47, 47);
+	mainFrame.add(shotCounter1);
+
+	JPanel shotCounter2 = new JPanel();
+	shotCounter2.setBounds(289, 11, 47, 47);
+	mainFrame.add(shotCounter2);
+
+	JPanel shotCounter3 = new JPanel();
+	shotCounter3.setBounds(341, 11, 47, 47);
+	mainFrame.add(shotCounter3);
+
+	JPanel shotCounter4 = new JPanel();
+	shotCounter4.setBounds(442, 764, 47, 47);
+	mainFrame.add(shotCounter4);
+
+	JPanel shotCounter5 = new JPanel();
+	shotCounter5.setBounds(499, 764, 47, 47);
+	mainFrame.add(shotCounter5);
+
+	JPanel shotCounter6 = new JPanel();
+	shotCounter6.setBounds(554, 764, 47, 47);
+	mainFrame.add(shotCounter6);
+
+	JPanel shotCounter7 = new JPanel();
+	shotCounter7.setBounds(823, 675, 47, 47);
+	mainFrame.add(shotCounter7);
+
+	JPanel shotCounter8 = new JPanel();
+	shotCounter8.setBounds(882, 675, 47, 47);
+	mainFrame.add(shotCounter8);
+
+	JPanel shotCounter9 = new JPanel();
+	shotCounter9.setBounds(1205, 683, 47, 47);
+	mainFrame.add(shotCounter9);
+
+	JPanel shotCounter10 = new JPanel();
+	shotCounter10.setBounds(1258, 683, 47, 47);
+	mainFrame.add(shotCounter10);
+
+	JPanel shotCounter11 = new JPanel();
+	shotCounter11.setBounds(1311, 683, 47, 47);
+	mainFrame.add(shotCounter11);
+
+	JPanel shotCounter12 = new JPanel();
+	shotCounter12.setBounds(1112, 23, 47, 47);
+	mainFrame.add(shotCounter12);
+
+	JPanel shotCounter13 = new JPanel();
+	shotCounter13.setBounds(1058, 23, 47, 47);
+	mainFrame.add(shotCounter13);
+
+	JPanel shotCounter14 = new JPanel();
+	shotCounter14.setBounds(1004, 23, 47, 47);
+	mainFrame.add(shotCounter14);
+
+	JPanel shotCounter15 = new JPanel();
+	shotCounter15.setBounds(642, 156, 47, 47);
+	mainFrame.add(shotCounter15);
+
+	JPanel shotCounter16 = new JPanel();
+	shotCounter16.setBounds(513, 277, 47, 47);
+	mainFrame.add(shotCounter16);
+
+	JPanel shotCounter17 = new JPanel();
+	shotCounter17.setBounds(513, 330, 47, 47);
+	mainFrame.add(shotCounter17);
+
+	JPanel shotCounter18 = new JPanel();
+	shotCounter18.setBounds(672, 473, 47, 47);
+	mainFrame.add(shotCounter18);
+
+	JPanel shotCounter19 = new JPanel();
+	shotCounter19.setBounds(725, 473, 47, 47);
+	mainFrame.add(shotCounter19);
+
+	JPanel shotCounter20 = new JPanel();
+	shotCounter20.setBounds(1040, 549, 47, 47);
+	mainFrame.add(shotCounter20);
+
+	JPanel shotCounter21 = new JPanel();
+	shotCounter21.setBounds(826, 216, 47, 47);
+	mainFrame.add(shotCounter21);
+
+	JPanel shotCounter22 = new JPanel();
+	shotCounter22.setBounds(879, 216, 47, 47);
+	mainFrame.add(shotCounter22);	
+    }
+
+}
--- src/Deadwood.java
+++ src/Deadwood.java
@@ -24,7 +24,7 @@ public class Deadwood{
 	mainFrame.setLayout(null);
 
 	createSceneCardPanels(mainFrame, board);
-	createShotCountersPanels(mainFrame, board);
+	CreateShotCountersPanels.main(mainFrame);
 	createRolePanels(mainFrame, board);
 	createSidePanel(mainFrame, board);
 	createBottomPanel(mainFrame);
@@ -100,98 +100,6 @@ public class Deadwood{
 	    }
 	    i++;
 	}
-    }
-
-    // Creates all the shot counter location panels
-    private static void createShotCountersPanels(JFrame mainFrame, Board board) throws IOException{
-
-	JPanel shotCounter1 = new JPanel();
-	shotCounter1.setBounds(236, 11, 47, 47);
-	mainFrame.add(shotCounter1);
-
-	JPanel shotCounter2 = new JPanel();
-	shotCounter2.setBounds(289, 11, 47, 47);
-	mainFrame.add(shotCounter2);
-
-	JPanel shotCounter3 = new JPanel();
-	shotCounter3.setBounds(341, 11, 47, 47);
-	mainFrame.add(shotCounter3);
-
-	JPanel shotCounter4 = new JPanel();
-	shotCounter4.setBounds(442, 764, 47, 47);
-	mainFrame.add(shotCounter4);
-
-	JPanel shotCounter5 = new JPanel();
-	shotCounter5.setBounds(499, 764, 47, 47);
-	mainFrame.add(shotCounter5);
-
-	JPanel shotCounter6 = new JPanel();
-	shotCounter6.setBounds(554, 764, 47, 47);
-	mainFrame.add(shotCounter6);
-
-	JPanel shotCounter7 = new JPanel();
-	shotCounter7.setBounds(823, 675, 47, 47);
-	mainFrame.add(shotCounter7);
-
-	JPanel shotCounter8 = new JPanel();
-	shotCounter8.setBounds(882, 675, 47, 47);
-	mainFrame.add(shotCounter8);
-
-	JPanel shotCounter9 = new JPanel();
-	shotCounter9.setBounds(1205, 683, 47, 47);
-	mainFrame.add(shotCounter9);
-
-	JPanel shotCounter10 = new JPanel();
-	shotCounter10.setBounds(1258, 683, 47, 47);
-	mainFrame.add(shotCounter10);
-
-	JPanel shotCounter11 = new JPanel();
-	shotCounter11.setBounds(1311, 683, 47, 47);
-	mainFrame.add(shotCounter11);
-
-	JPanel shotCounter12 = new JPanel();
-	shotCounter12.setBounds(1112, 23, 47, 47);
-	mainFrame.add(shotCounter12);
-
-	JPanel shotCounter13 = new JPanel();
-	shotCounter13.setBounds(1058, 23, 47, 47);
-	mainFrame.add(shotCounter13);
-
-	JPanel shotCounter14 = new JPanel();
-	shotCounter14.setBounds(1004, 23, 47, 47);
-	mainFrame.add(shotCounter14);
-
-	JPanel shotCounter15 = new JPanel();
-	shotCounter15.setBounds(642, 156, 47, 47);
-	mainFrame.add(shotCounter15);
-
-	JPanel shotCounter16 = new JPanel();
-	shotCounter16.setBounds(513, 277, 47, 47);
-	mainFrame.add(shotCounter16);
-
-	JPanel shotCounter17 = new JPanel();
-	shotCounter17.setBounds(513, 330, 47, 47);
-	mainFrame.add(shotCounter17);
-
-	JPanel shotCounter18 = new JPanel();
-	shotCounter18.setBounds(672, 473, 47, 47);
-	mainFrame.add(shotCounter18);
-
-	JPanel shotCounter19 = new JPanel();
-	shotCounter19.setBounds(725, 473, 47, 47);
-	mainFrame.add(shotCounter19);
-
-	JPanel shotCounter20 = new JPanel();
-	shotCounter20.setBounds(1040, 549, 47, 47);
-	mainFrame.add(shotCounter20);
-
-	JPanel shotCounter21 = new JPanel();
-	shotCounter21.setBounds(826, 216, 47, 47);
-	mainFrame.add(shotCounter21);
-
-	JPanel shotCounter22 = new JPanel();
-	shotCounter22.setBounds(879, 216, 47, 47);
-	mainFrame.add(shotCounter22);
     }
 
     // Makes the panel that displays the Deadwood game background
