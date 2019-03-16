8f40bba7f92dc83bca26a5a424b10e226576b777
Thomas <thomasjonesmoore@gmail.com>
2019-03-15 21:41:57 -0700

    baseline for daykeeper

--- src/DayKeeper.java
+++ src/DayKeeper.java
@@ -1,26 +1,26 @@
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.image.*;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.util.*;
+import java.awt.BorderLayout;
+import java.util.concurrent.*;
 
 public class DayKeeper{
 
     Board board;
+    int day;
 
     public DayKeeper(Board board){
 	this.board = board;
+	day = 1;
+	
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void nextDay(){
+	day++;
+    }
 }
